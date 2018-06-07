2607dc3e70c5311d6f364ff9e9827d19d42d678c
cathy-zou <511945062@qq.com>
2018-06-07 14:33:16 +0800

    微信小程序原生练习

---
+++ pages/index/index.js
@@ -0,0 +1,19 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    grids: [
+      {id: 1, name: '川菜', img: '../../image/1.png'},
+      {id: 2,  name: '湘菜', img: '../../image/2.png' },
+      {id: 3,  name: '粤菜', img: '../../image/3.png'},
+      {id: 4, name: '火锅', img: '../../image/4.png' },
+      {id: 5, name: '自助餐', img: '../../image/5.png' },
+      {id: 6, name: '烤鱼', img: '../../image/6.png' },
+      {id: 7, name: '西餐', img: '../../image/7.png' },
+      {id: 8, name: '快餐', img: '../../image/8.png' },
+      {id: 9, name: '外卖', img: '../../image/9.png'}
+    ]
+  }
+});
